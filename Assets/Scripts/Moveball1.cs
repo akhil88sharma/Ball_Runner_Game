8843773bc84310be8a2bdf4a4a9d6d36e4fd7f23
akhil88sharma <akhil88sharma@gmail.com>
2024-01-13 13:47:32 +0530

    Game Prototype

---
+++ Assets/Scripts/Moveball1.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+
+public class Moveball1 : MonoBehaviour
+{
+    Rigidbody rb;
+    public int ballspeed;
+    public int jumpspeed;
+    bool istouching = true;
+    public Text Coinstext;
+    int counter;
+    public AudioClip aclip;
+    public AudioSource asource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        counter = 0;
+        Coinstext.text = "Coins:" + counter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Hmove = Input.GetAxis("Horizontal");
+        float Vmove = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(Hmove, 0.0f, Vmove);
+        rb.AddForce(move * ballspeed);
+        bool ifjump = Input.GetKey(KeyCode.Space);
+
+
+        if ((ifjump) && (istouching))
+        {
+            Vector3 jump = new Vector3(0.0f, 10, 0.0f);
+            rb.AddForce(jump * jumpspeed);
+            istouching = false;
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        istouching = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Cointag"))
+        {
+            other.gameObject.SetActive(false);
+            counter++;
+            Coinstext.text = "Coins:" + counter;
+            asource.PlayOneShot(aclip);
+        }
+        if (counter == 9)
+        {
+            SceneManager.LoadScene("level 2");
+        }
+    }
+}
---
+++ Assets/Scripts/Movecamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movecamera : MonoBehaviour
+{
+    public GameObject ball;
+    Vector3 dist;// Start is called before the first frame update
+    void Start()
+    {
+        dist = transform.position - ball.transform.position;
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = ball.transform.position + dist;
+    }
+}
